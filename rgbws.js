2191648b36347d49becb4d89b4bfcea7d80a6fdb
Olivia Chang <15709895+oliviachang29@users.noreply.github.com>
2020-07-15 08:13:32 -0700

    add wakeup capability

--- rgbws.js
+++ rgbws.js
@@ -1,7 +1,10 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
-var Gpio = require('pigpio').Gpio, //include pigpio to interact with the GPIO
+var Gpio = require('pigpio').Gpio //include pigpio to interact with the GPIO
+var schedule = require('node-schedule');
+var moment = require('moment'); // require
+
 // using BCM numbering: https://pinout.xyz/
 ledRed = new Gpio(17, {mode: Gpio.OUTPUT}), //use GPIO pin 4 as output for RED
 ledGreen = new Gpio(27, {mode: Gpio.OUTPUT}), //use GPIO pin 17 as output for GREEN
@@ -15,16 +18,36 @@ ledRed.digitalWrite(0); // Turn RED LED off
 ledGreen.digitalWrite(0); // Turn GREEN LED off
 ledBlue.digitalWrite(0); // Turn BLUE LED off
 
+// set up wake up job
+var wakeupRule = new schedule.RecurrenceRule();
+wakeupRule.hour = 7;
+wakeupRule.minute = 30;
+// wakeupRule.second = 1; // use this for testing
+
+wakeupColor = {r: 0 , g: 251, b: 255}
+
+var wakeupJob = schedule.scheduleJob(wakeupRule, wakeup);
+
+// bedtime
+
+var bedtimeRule = new schedule.RecurrenceRule();
+bedtimeRule.hour = 21;
+bedtimeRule.minute = 0;
+
+bedtimeColor = { r: 4, g: 0, b: 0 }
+
+var bedtimeJob = schedule.scheduleJob(bedtimeRule, bedtime);
+
 http.listen(8080); //listen to port 8080
 
 function handler (req, res) { //what to do on requests to port 8080
-  fs.readFile(__dirname + '/public/rgb.html', function(err, data) { //read file rgb.html in public folder
+  fs.readFile(__dirname + '/public/index.html', function(err, data) { //read file index.html in public folder
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
       return res.end("404 Not Found");
     }
     res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from rgb.html
+    res.write(data); //write data from index.html
     return res.end();
   });
 }
@@ -35,22 +58,44 @@ function changeColor(r, g, b) {
   ledBlue.pwmWrite(b); //set BLUE LED to specified value
 }
 
+function wakeup() {
+  changeColor(wakeupColor.r, wakeupColor.g, wakeupColor.b)
+}
+
+function bedtime() {
+  changeColor(bedtimeColor.r, bedtimeColor.g, bedtimeColor.b)
+}
+
 io.sockets.on('connection', function (socket) {// Web Socket Connection
   socket.on('rgbLed', function(data) { //get light switch status from client
     // console.log(data); //output data from WebSocket connection to console
-
-    //for common cathode RGB LED 0 is fully off, and 255 is fully on
-    redRGB=parseInt(data.red);
-    greenRGB=parseInt(data.green);
-    blueRGB=parseInt(data.blue);
-
-    changeColor(redRGB, greenRGB, blueRGB)    
+    changeColor(data.red, data.green, data.blue)    
   });
 
-  // socket.on('setTime', function(type='wakeup', time) { // type is 'wakeup' or 'bedtime'
-  //   console.log("type: " + type)
-  //   console.log("time: " + time)
-  // })
+  socket.on('wakeupTime', function (time) {
+    console.log(time)
+
+    var d = new moment(time, ['h:m a', 'H:m'])
+
+    wakeupRule.hour = d.format('H');
+    wakeupRule.minute = d.format('m');
+
+    wakeupJob.reschedule(wakeupRule, wakeup);
+  });
+
+  socket.on('wakeupColor', function (data) {
+    wakeupColor = {
+      r: data.red,
+      g: data.blue,
+      b: data.green
+    }
+  });
+
+  socket.on('swatch', function (rgbObject) {
+    console.log(rgbObject);
+    changeColor(rgbObject.r, rgbObject.g, rgbObject.b);
+  });
+
 });
 
 process.on('SIGINT', function () { //on ctrl+c
